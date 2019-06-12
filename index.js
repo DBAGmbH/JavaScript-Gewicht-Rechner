b9b4dfc394fe36f2b418c027716095b1c831cd55
Denis <denis.bauer1@gmx.net>
2019-06-12 11:06:01 +0200

    erster versuch javascript

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 var grammInput = document.getElementById("grammInput");
+var kgOutPut = document.getElementById("kgOutPut");
+var tonneOutPut = document.getElementById("tonneOutPut");
+var mgOutPut = document.getElementById("mgOutPut");
 
-grammInput.addEventListener("input", function(){
-  console.log("123");
-})
+function Rechnen(value) {
+  if (typeof value != Number) {
+    window.alert("Falsche Eingabe Gebe eine Zahl ein")
+  }
+
+  kgOutPut.innerHTML = parseInt(value) / 1000;
+  tonneOutPut.innerHTML = parseInt(value) / 1000000;
+  mgOutPut.innerHTML = parseInt(value) * 1000;
+
+}
+
+grammInput.addEventListener("input", Rechnen);
+
+
+
