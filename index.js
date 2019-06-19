d082537445a96d4841403c946c80e191e1cf3b9e
Denis <Denis.bauer1@gmx.net>
2019-06-19 11:13:07 +0200

    hinzugefügt

--- index.js
+++ index.js
@@ -2,20 +2,11 @@ var grammInput = document.getElementById("grammInput");
 var kgOutPut = document.getElementById("kgOutPut");
 var tonneOutPut = document.getElementById("tonneOutPut");
 var mgOutPut = document.getElementById("mgOutPut");
+var pfundOutPut = document.getElementById("pfundOutPut");
+var mikrogrammOutPut = document.getElementById("mikrogrammOutPut");
+  
+  function Rechnen () {
+    parseInt(grammInput / 1000);
 
-function Rechnen(value) {
-  if (typeof value != Number) {
-    window.alert("Falsche Eingabe Gebe eine Zahl ein")
-  }
 
-  else {
-  kgOutPut.innerHTML = parseInt(value) / 1000;
-  tonneOutPut.innerHTML = parseInt(value) / 1000000;
-  mgOutPut.innerHTML = parseInt(value) * 1000;
 }
-}
-
-grammInput.addEventListener("input", Rechnen(grammInput));
-
-
-
