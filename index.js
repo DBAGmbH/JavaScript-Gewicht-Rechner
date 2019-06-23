ef81895bcd54c1047b5d12dd6688dde03864d8d1
rithbuster <Denis.Bauer1@gmx.net>
2019-06-23 14:49:27 +0200

    Rechner programmiert

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-let grammInput = document.getElementById("grammInput");
-let kgOutPut = document.getElementById("kgOutPut");
-let tonneOutPut = document.getElementById("tonneOutPut");
-let mgOutPut = document.getElementById("mgOutPut");
-let pfundOutPut = document.getElementById("pfundOutPut");
-let mikrogrammOutPut = document.getElementById("mikrogrammOutPut");
-let rechnenButton = document.getElementById("rechnenButton");
-
-function buttonClicked() {
-  if (typeof Zahl != "Number") {
-    window.alert("Gib Eine Gültige Zahl ein!")
-  } else {
-    kgOutPut.innerHTML = parseInt(document.grammInput.value / 1000);
-    tonneOutPut.innerHTML = parseInt(document.grammInput.value / 1000000);
-    mgOutPut.innerHTML = parseInt(document.grammInput.value * 1000);
-    pfundOutPut.innerHTML = parseInt(document.grammInput.value / 453.592);
-    mikrogrammOutPut.innerHTML = parseInt(document.grammInput.value * 1000000);
-  }
+function buttonClicked(Zahl) {
+  document.getElementById("kgOutPut").innerHTML = Zahl / 1000;
+  document.getElementById("tonneOutPut").innerHTML = Zahl / 1000000;
+  document.getElementById("mgOutPut").innerHTML = Zahl * 1000;
+  document.getElementById("pfundOutPut").innerHTML = Zahl / 453592;
+  document.getElementById("mikrogrammOutPut").innerHTML = Zahl * 1000000;
 }
