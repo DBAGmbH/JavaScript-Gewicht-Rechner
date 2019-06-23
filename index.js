c389286468d5fd86810c23ad4855ca0a53a0457e
rithbuster <Denis.Bauer1@gmx.net>
2019-06-23 15:45:19 +0200

    .min hinzugefügt die Zahl geht nichtmehr unter 0

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function buttonClicked(Zahl) {
+  document.getElementById("grammInput").min = "0";
   document.getElementById("kgOutPut").innerHTML = Zahl / 1000;
   document.getElementById("tonneOutPut").innerHTML = Zahl / 1000000;
   document.getElementById("mgOutPut").innerHTML = Zahl * 1000;
